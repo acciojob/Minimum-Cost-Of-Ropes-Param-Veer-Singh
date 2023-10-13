40197995359e88659c1ec6b881173b51d7e79920
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-13 11:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function calculateMinCost() {
-  //your code here
+    //your code here
+    let arr = document.getElementById("rope-lengths").value.split(",");
+    
+    let totalCost = 0;
+    arr.sort((a,b) => a-b);
+    while(arr.length > 1){
+
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let cost = (parseInt(first) + parseInt(second));
+        totalCost += cost;
+        arr.push(cost);
+
+        arr.sort((a,b) => a-b);
+    }
+
+    // console.log(totalCost);
+    return totalCost;
+    
+  }  
   
-  
-  
-}  
